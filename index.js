5a3e133612cded83547bf2159979e72edd070cc3
Raul Barroso <ra.barroso@gmail.com>
2015-02-05 14:24:26 -0800

    This at least does something :turtle:

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var exec = require('child_process').exec;
+var child;
+
 exports.topics = [{
   name: '_orgs',
   description: 'CLI for Heroku Organizations'
@@ -10,7 +13,14 @@ exports.commands = [
     description: 'Lists the organizations you are a member of',
     help: 'Example: heroku _orgs:list',
     run: function () {
-      console.log('# TODO: Do what it\'s expected');
+      child = exec('heroku orgs',
+        function (error, stdout, stderr) {
+          console.log(stdout);
+          if (error !== null) {
+            console.log('stderr: ' + stderr);
+            console.log('exec error: ' + error);
+          }
+      });
     }
   }
 ];
