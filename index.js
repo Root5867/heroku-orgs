9178758559b67a4655d8b37cca89619faf14efff
Raul Barroso <ra.barroso@gmail.com>
2015-02-05 22:59:34 -0800

    List organizations from the API

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-var exec = require('child_process').exec;
-var child;
-
 exports.topics = [{
   name: '_orgs',
   description: 'CLI for Heroku Organizations'
 }];
 
 exports.commands = [
-  {
-    topic: '_orgs',
-    command: 'list',
-    description: 'Lists the organizations you are a member of',
-    help: 'Example: heroku _orgs:list',
-    run: function () {
-      child = exec('heroku orgs',
-        function (error, stdout, stderr) {
-          console.log(stdout);
-          if (error !== null) {
-            console.log('stderr: ' + stderr);
-            console.log('exec error: ' + error);
-          }
-      });
-    }
-  }
+  require('./lib/commands/orgs/list')
 ];
