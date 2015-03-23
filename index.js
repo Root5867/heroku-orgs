a9d47a106619c44f005e779baf79071950ce0939
Raul Barroso <ra.barroso@gmail.com>
2015-03-22 21:00:26 -0700

    List privileges in org apps

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ exports.topics = [{
 
 exports.commands = [
   require('./lib/commands/orgs/list'),
-  require('./lib/commands/apps/list')
+  require('./lib/commands/apps/list'),
+  require('./lib/commands/access/list')
 ];
