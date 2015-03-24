00a43e3d5280028671b713d5a10f77e34e2475f2
Raul Barroso <ra.barroso@gmail.com>
2015-03-24 15:18:08 -0700

    Be able to add/remove collaborators in personal apps

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ exports.topics = [{
 exports.commands = [
   require('./lib/orgs'),
   require('./lib/apps'),
-  require('./lib/access/list')
+  require('./lib/access/list'),
+  require('./lib/access/add'),
+  require('./lib/access/remove')
 ];
